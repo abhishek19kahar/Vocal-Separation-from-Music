3c2162115e77042e8e5777c2121a0e157b6cdd11
abhishek19kahar <125858848+abhishek19kahar@users.noreply.github.com>
2025-01-15 00:41:14 +0530

    Update AuthRouter.js

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,13 +1,3 @@
-// const { signup, login } = require('../Controllers/AuthController');
-// const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
-
-// const router =require('express').Router();
-
-// router.post('/login', loginValidation, login);
-
-// router.post('/signup', signupValidation, signup);
-// module.exports = router;
-
 import { Router } from 'express';
 import { signup, login } from '../Controllers/AuthController.js';
 import { signupValidation, loginValidation } from '../Middlewares/AuthValidation.js';
