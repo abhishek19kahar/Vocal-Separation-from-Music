bae81c86a61a554ae1b9d5c955d1ef9c4cf9fce5
abhishek19kahar <125858848+abhishek19kahar@users.noreply.github.com>
2025-01-15 00:41:29 +0530

    Update ContactRouter.js

--- backend/Routes/ContactRouter.js
+++ backend/Routes/ContactRouter.js
@@ -1,14 +1,3 @@
-// const express = require('express');
-// const { submitContactForm } = require('../Controllers/ContactController');
-
-// const router = express.Router();
-
-// // POST /api/contact
-// router.post('/contact', submitContactForm);
-
-// module.exports = router;
-
-
 import express from 'express';
 import { submitContactForm } from '../Controllers/ContactController.js';
 
